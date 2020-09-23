35d2e5ff4fc3a3c2d9e46759b84caad9f5563dd3
suneeil007 <suneeil.me@gmail.com>
2020-09-23 11:53:37 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import  Name  from './index';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
